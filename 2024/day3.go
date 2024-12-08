cb07b2e794e1818eb774591d45ec62f7aa01ec39
Nicolas Colomer <ncolomer@users.noreply.github.com>
2024-12-08 17:49:41 +0100

    Add 2024 day3

---
+++ 2024/day3.go
@@ -0,0 +1,55 @@
+package adventofcode2024
+
+import (
+	"bufio"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type Instruction struct {
+	enabled  bool
+	operandA int
+	operandB int
+}
+
+func Day3Read(input string) (instructions []Instruction, err error) {
+	reader := strings.NewReader(input)
+	scanner := bufio.NewScanner(reader)
+	re := regexp.MustCompile(`(do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\))`)
+	enabled := true
+	for scanner.Scan() {
+		line := scanner.Text()
+		for _, match := range re.FindAllStringSubmatch(line, -1) {
+			switch {
+			case match[1] == "do()":
+				enabled = true
+			case match[1] == "don't()":
+				enabled = false
+			default:
+				operandA, _ := strconv.Atoi(match[2])
+				operandB, _ := strconv.Atoi(match[3])
+				instructions = append(instructions, Instruction{enabled, operandA, operandB})
+			}
+		}
+	}
+	return
+}
+
+// SolveDay3Part1 compute sum of instructions' result
+func SolveDay3Part1(instructions []Instruction) (res int, err error) {
+	for _, i := range instructions {
+		res += i.operandA * i.operandB
+	}
+	return
+}
+
+// SolveDay3Part2 compute sum of instructions' result if enabled
+func SolveDay3Part2(instructions []Instruction) (res int, err error) {
+	for _, i := range instructions {
+		if i.enabled {
+			res += i.operandA * i.operandB
+		}
+	}
+	return
+}
---
+++ 2024/day3_test.go
@@ -0,0 +1,63 @@
+package adventofcode2024
+
+import (
+	_ "embed"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+var (
+	//go:embed testdata/day3_sample1.txt
+	Day3Sample1 string
+	//go:embed testdata/day3_sample2.txt
+	Day3Sample2 string
+	//go:embed testdata/day3_puzzle.txt
+	Day3Puzzle string
+)
+
+func TestDay3Part1Sample(t *testing.T) {
+	// Given
+	instructions, err := Day3Read(Day3Sample1)
+	require.NoError(t, err)
+	// When
+	sum, err := SolveDay3Part1(instructions)
+	require.NoError(t, err)
+	// then
+	assert.Equal(t, 161, sum)
+}
+
+func TestDay3Part1Puzzle(t *testing.T) {
+	// Given
+	instructions, err := Day3Read(Day3Puzzle)
+	require.NoError(t, err)
+	// When
+	sum, err := SolveDay3Part1(instructions)
+	require.NoError(t, err)
+	// then
+	assert.Equal(t, 183788984, sum)
+	t.Logf("day 3 part 1 result is: %d", sum)
+}
+
+func TestDay3Part2Sample(t *testing.T) {
+	// Given
+	instructions, err := Day3Read(Day3Sample2)
+	require.NoError(t, err)
+	// When
+	sum, err := SolveDay3Part2(instructions)
+	require.NoError(t, err)
+	// then
+	assert.Equal(t, 48, sum)
+}
+
+func TestDay3Part2Puzzle(t *testing.T) {
+	// Given
+	instructions, err := Day3Read(Day3Puzzle)
+	require.NoError(t, err)
+	// When
+	sum, err := SolveDay3Part2(instructions)
+	require.NoError(t, err)
+	// then
+	assert.Equal(t, 62098619, sum)
+	t.Logf("day 3 part 2 result is: %d", sum)
+}
